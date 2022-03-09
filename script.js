45d1696ae15b4f8f0c9c3b2cbb19c06797f7b437
Richardparkour <99192524+Richardparkour@users.noreply.github.com>
2022-03-09 18:35:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//data types//
+var string="welcome"
+var num=25
+var float=2.5
+var boolean=true
+
+console.log(string + " to JavaScript")
+console.log(num + " is the example for integer data type")
+console.log(float + " is the example for float data type")
+console.log(boolean)
+
+//Area of a cricle//
+var r=25
+let π=3.14
+const A=π*(r*r)
+console.log("Area of a circle is " + A)
+
+//Circumference of a circle//
+var r=15
+const C=2*π*r
+console.log("Circumference of circle is " + C)
+ 
+//Radius of the circle//
+const R=C/(2*π)
+console.log("Radius of the circle is " + R)
